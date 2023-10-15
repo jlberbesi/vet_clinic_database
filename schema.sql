5b223b2fb52a2a1a99fecab364c94843b2625d68
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-10-14 19:47:02 -0500

    Add updates on data, queries, and shema sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+/* Database schema to keep the structure of entire database. */
+
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -29,3 +31,28 @@ ALTER TABLE animals
   ADD COLUMN owner_id INT,
   ADD FOREIGN KEY (species_id) REFERENCES species(id),
   ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+  /*day4*/
+
+  CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  age INTEGER NOT NULL,
+  date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+  vet_id INTEGER NOT NULL,
+  species_id INTEGER NOT NULL,
+  PRIMARY KEY (vet_id, species_id),
+  FOREIGN KEY (vet_id) REFERENCES vets (id),
+  FOREIGN KEY (species_id) REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+  animal_id INTEGER NOT NULL,
+  vet_id INTEGER NOT NULL,
+  visit_date DATE NOT NULL,
+  FOREIGN KEY (animal_id) REFERENCES animals (id),
+  FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
